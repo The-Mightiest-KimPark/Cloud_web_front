698373fc6559a4dc7198f6747a544f00ca2b1376
Sangji Lee <mchy3018@gmail.com>
2020-11-27 18:17:00 +0900

    react-practice

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,15 @@
+import React, {Components} from 'react';
+
+class TodoItemList extends Components {
+    render() {
+        const {todos, onToggle, onRemove} = this.props;
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;
